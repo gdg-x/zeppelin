0d3ee33aeb02318468522181330d800877b3bb22
Nicolas Mingo <nicolas.mingo@neomades.com>
2017-09-04 13:01:24 +0200

    Add 2nd event place for gmaps

--- js/scripts.js
+++ js/scripts.js
@@ -124,7 +124,7 @@
         });
         $('#tickets').appear(function() {
             ga('send', 'event', 'Tickets', 'appear');
-        }
+        });
         $('.animated').appear(function() {
             var element = $(this);
             var animation = element.data('animation');
@@ -283,6 +283,9 @@
     // Google maps static
     if (typeof staticGoogleMaps !== 'undefined') {
         $('#canvas-map').addClass('image-section').css('background-image','url(https://maps.googleapis.com/maps/api/staticmap?zoom=17&center=' + mobileCenterMapCoordinates +'&size=' + $(window).width() + 'x700&scale=2&language=en&markers=icon:' + icon +'|'+ eventPlaceCoordinates +'&maptype=roadmap&style=visibility:on|lightness:40|gamma:1.1|weight:0.9&style=element:labels|visibility:off&style=feature:water|hue:0x0066ff&style=feature:road|visibility:on&style=feature:road|element:labels|saturation:-30)');
+    }
+    if (typeof staticGoogleMaps !== 'undefined') {
+        $('#canvas-map2').addClass('image-section').css('background-image','url(https://maps.googleapis.com/maps/api/staticmap?zoom=17&center=' + mobileCenterMapCoordinates +'&size=' + $(window).width() + 'x700&scale=2&language=en&markers=icon:' + icon +'|'+ event2PlaceCoordinates +'&maptype=roadmap&style=visibility:on|lightness:40|gamma:1.1|weight:0.9&style=element:labels|visibility:off&style=feature:water|hue:0x0066ff&style=feature:road|visibility:on&style=feature:road|element:labels|saturation:-30)');
     }
 
     //Google maps
