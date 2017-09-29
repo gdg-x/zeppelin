a7805f1a1af870cdedf93d1f0542238ed475d8a0
Nick Olson <niolson@wharton.upenn.edu>
2017-09-29 00:25:19 -0400

    Resolved static GMap issue

--- js/scripts.js
+++ js/scripts.js
@@ -280,7 +280,8 @@
 
     // Google maps static
     if (typeof staticGoogleMaps !== 'undefined') {
-        $('#canvas-map').addClass('image-section').css('background-image','url(https://maps.googleapis.com/maps/api/staticmap?key=AIzaSyBa5SNPn4d9kt2cK3uH4lsKHLfebp5p3rI&center=' + mobileCenterMapCoordinates +'&zoom=17&size=' + $(window).width() + 'x700&scale=2&language=en&markers=icon:' + icon +'|'+ eventPlaceCoordinates +'&maptype=roadmap&style=visibility:on|lightness:40|gamma:1.1|weight:0.9&style=element:labels|visibility:off&style=feature:water|hue:0x0066ff&style=feature:road|visibility:on&style=feature:road|element:labels|saturation:-30)');
+        //$('#canvas-map').addClass('image-section').css('background-image','url(https://maps.googleapis.com/maps/api/staticmap?key=AIzaSyBa5SNPn4d9kt2cK3uH4lsKHLfebp5p3rI&center=' + mobileCenterMapCoordinates +'&zoom=17&size=' + $(window).width() + 'x700&scale=2&language=en&markers=icon:' + icon +'|'+ eventPlaceCoordinates +'&maptype=roadmap&style=visibility:on|lightness:40|gamma:1.1|weight:0.9&style=element:labels|visibility:off&style=feature:water|hue:0x0066ff&style=feature:road|visibility:on&style=feature:road|element:labels|saturation:-30)');
+        $('#canvas-map').addClass('image-section').css('background-image','url(https://maps.googleapis.com/maps/api/staticmap?key=AIzaSyBa5SNPn4d9kt2cK3uH4lsKHLfebp5p3rI&center=' + mobileCenterMapCoordinates +'&zoom=17&size=' + $(window).width() + 'x700&scale=2&language=en&markers=color:red|'+ eventPlaceCoordinates +'&maptype=roadmap&style=visibility:on|lightness:40|gamma:1.1|weight:0.9&style=element:labels|visibility:off&style=feature:water|hue:0x0066ff&style=feature:road|visibility:on&style=feature:road|element:labels|saturation:-30)');
     }
 
     //Google maps
