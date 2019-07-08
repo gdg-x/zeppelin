cd2d4aba56b0b418e75bc12cd8acbded19974770
Jordan Simonovski <jordan.simonovski@lendi.com.au>
2019-07-08 23:51:42 +1000

    removed the google loading thing

--- js/scripts.js
+++ js/scripts.js
@@ -3,7 +3,7 @@
         $(window).load(function() {
             $('#st-container').removeClass('disable-scrolling');
             $('#loading-animation').fadeOut();
-            $('#preloader').delay(350).fadeOut(800);
+            // $('#preloader').delay(350).fadeOut(800);
             initGooglePlus();
             equalheight('.same-height');
         });
